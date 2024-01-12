e1387f86a36dbe353a63e039d19ed1713cb546f0
Esra Aydemir <es.altintas@gmail.com>
2024-01-13 00:23:59 +0300

    Add header and footer

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import styles from "./header.module.css";
+import PlanALogo from "../../assets/images/plana_logo.svg";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, react/function-component-definition
+export default function Header() {
+  return (
+    <div className={`${styles.headerWrapper}`} data-testid="header">
+      <Link to={"/"}>
+        <LazyLoadImage
+          src={PlanALogo}
+          width={80}
+          height={80}
+          alt="Plan A Logo"
+        />
+      </Link>
+    </div>
+  );
+}
