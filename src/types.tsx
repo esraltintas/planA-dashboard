f4d6999a804508482a1f989be1a1b0d6ec8a4386
Esra Aydemir <es.altintas@gmail.com>
2024-01-14 00:32:27 +0300

    Add all project types

---
+++ src/types.tsx
@@ -0,0 +1,42 @@
+export interface Product {
+  name: string;
+  description: string;
+  product_variable: string;
+}
+
+export interface Statistic {
+  time: {
+    interval_start: string;
+    max: string;
+    min: string;
+  };
+  value: {
+    average: number;
+    count: number;
+    max: number;
+    min: number;
+    standard_deviation: number;
+  };
+}
+
+export interface ProductStatisticGetParams {
+  selectedGHG: string;
+  startDate: Date | null;
+  endDate: Date | null;
+}
+
+export interface ChartProps {
+  chartData: any[];
+}
+
+export interface GHGSelectorProps {
+  selectedGHG: string;
+  ghgOptions: { value: string; label: string }[];
+  onChange: (selectedGHG: string) => void;
+}
+
+export interface DashboardCardProps {
+  title: string;
+  description: string;
+  imageUrl?: string;
+}
